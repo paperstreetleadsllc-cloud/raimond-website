641ca95b4f4c88c013eead3f2b1f0fa785344a0d
Bobby Axelrod <you@example.com>
2025-10-23 17:13:16 -0400

    Clean Home.tsx (remove stray text) and proper imports/layout

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,1 +1,20 @@
-C:\Users\drews
+﻿import React from "react";
+import Hero from "../sections/Hero";
+import FeatureHighlights from "../sections/FeatureHighlights";
+import CapabilitiesGrid from "../sections/CapabilitiesGrid";
+import CTA from "../sections/CTA";
+import Newsletter from "../sections/Newsletter";
+import Footer from "../shared/Footer";
+
+export default function Home() {
+  return (
+    <>
+      <Hero />
+      <FeatureHighlights />
+      <CapabilitiesGrid />
+      <CTA />
+      <Newsletter />
+      <Footer />
+    </>
+  );
+}
