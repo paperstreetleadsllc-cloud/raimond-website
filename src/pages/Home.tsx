c3a14dc6da2b99ffb5d4eb1a4d3d824f705fb3a8
Bobby Axelrod <you@example.com>
2025-10-23 11:22:26 -0400

    Fix Home.tsx corruption; add FeatureHighlights + CapabilitiesGrid to Home

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,1 +1,14 @@
-C:\Users\drews
+import React from "react";
+import Hero from "../sections/Hero";
+import FeatureHighlights from "../sections/FeatureHighlights";
+import CapabilitiesGrid from "../sections/CapabilitiesGrid";
+
+export default function Home() {
+  return (
+    <>
+      <Hero />
+      <FeatureHighlights />
+      <CapabilitiesGrid />
+    </>
+  );
+}
