3e17efcdf6cf8fc4e824dd887db7adda189aca6f
Bobby Axelrod <you@example.com>
2025-10-24 11:47:45 -0400

    Remove duplicate footer from Home

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -3,16 +3,12 @@ import Hero from "../sections/Hero";
 import FeatureHighlights from "../sections/FeatureHighlights";
 import CapabilitiesGrid from "../sections/CapabilitiesGrid";
 import CTA from "../sections/CTA";
-import Footer from "../shared/Footer";
-
 export default function Home() {
   return (
     <>
       <Hero />
       <FeatureHighlights />
       <CapabilitiesGrid />
-      <CTA />
-      <Footer />
-    </>
+      <CTA /></>
   );
 }
